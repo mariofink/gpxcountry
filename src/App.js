89b4d67686afa26546584b49e3e1777fc6448d2e
Mario Fink <mario.fink@goodyear.com>
2021-10-23 23:03:07 +0200

    render markers on map

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
             onClick={(latlng) =>
               setWaypoints((prevWaypoints) => [...prevWaypoints, latlng])
             }
+            waypoints={waypoints}
           />
         </div>
       </div>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,19 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "leaflet/dist/leaflet.css";
-import { map, tileLayer } from "leaflet";
+import { map, tileLayer, circleMarker, LayerGroup } from "leaflet";
 
-const Map = ({ lat, lng, onClick }) => {
+const Map = ({ lat, lng, onClick, waypoints }) => {
+  const osMap = useRef(null);
+  const markersLayer = useRef(null);
   useEffect(() => {
-    const osMap = map("leafletmap").setView([lat, lng], 13);
+    osMap.current = map("leafletmap").setView([lat, lng], 13);
+    markersLayer.current = new LayerGroup();
+    markersLayer.current.addTo(osMap.current);
     tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       useCache: true,
-    }).addTo(osMap);
-    osMap.on("click", (e) => {
+    }).addTo(osMap.current);
+    osMap.current.on("click", (e) => {
       onClick(e.latlng);
     });
   }, []);
+  useEffect(() => {
+    markersLayer.current.clearLayers();
+    waypoints.map((waypoint) => {
+      console.log("add marker", waypoint);
+      const marker = circleMarker([waypoint.lat, waypoint.lng]);
+      markersLayer.current.addLayer(marker);
+    });
+  }, [waypoints]);
   return <div id="leafletmap" className="h-full"></div>;
 };
 

953620915a4cc941ba865417b14d979c81f354e3
Mario Fink <mario.fink@goodyear.com>
2021-10-24 22:20:25 +0200

    extract icons to separate file

--- src/components/Waypoints/Waypoints.jsx
+++ src/components/Waypoints/Waypoints.jsx
@@ -1,43 +1,4 @@
-/**
- * Icons from here: https://iconsvg.xyz/
- */
-
-const BurgerIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <line x1="3" y1="12" x2="21" y2="12"></line>
-    <line x1="3" y1="6" x2="21" y2="6"></line>
-    <line x1="3" y1="18" x2="21" y2="18"></line>
-  </svg>
-);
-
-const BinIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <polyline points="3 6 5 6 21 6"></polyline>
-    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-    <line x1="10" y1="11" x2="10" y2="17"></line>
-    <line x1="14" y1="11" x2="14" y2="17"></line>
-  </svg>
-);
+import { BinIcon, BurgerIcon } from "../Icons/Icons";
 
 const Waypoints = ({ list }) => (
   <div className="flex flex-col justify-between bg-gray-700 text-white p-4">

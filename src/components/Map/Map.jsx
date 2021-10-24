d4a52fd06b0cfe357c1f59705a8f96838e30d75d
Mario Fink <mario.fink@goodyear.com>
2021-10-24 22:35:34 +0200

    use separate layer for line drawing

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,11 +5,14 @@ import { map, tileLayer, marker, LayerGroup, divIcon, polyline } from "leaflet";
 const Map = ({ lat, lng, onClick, waypoints }) => {
   const osMap = useRef(null);
   const markersLayer = useRef(null);
+  const lineLayer = useRef(null);
   // init map
   useEffect(() => {
     osMap.current = map("leafletmap").setView([lat, lng], 13);
     markersLayer.current = new LayerGroup();
     markersLayer.current.addTo(osMap.current);
+    lineLayer.current = new LayerGroup();
+    lineLayer.current.addTo(osMap.current);
     tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -22,6 +25,7 @@ const Map = ({ lat, lng, onClick, waypoints }) => {
   // update map
   useEffect(() => {
     markersLayer.current.clearLayers();
+    lineLayer.current.clearLayers();
     waypoints.map((waypoint, index) => {
       const markerIcon = divIcon({
         className: "map-marker text-white bg-gray-700",
@@ -35,7 +39,8 @@ const Map = ({ lat, lng, onClick, waypoints }) => {
       markersLayer.current.addLayer(mapMarker);
     });
     if (waypoints.length > 1) {
-      polyline(waypoints, { color: "#1086e8", weight: 6 }).addTo(osMap.current);
+      const line = polyline(waypoints, { color: "#1086e8", weight: 6 });
+      lineLayer.current.addLayer(line);
     }
   }, [waypoints]);
   return <div id="leafletmap" className="h-full"></div>;
